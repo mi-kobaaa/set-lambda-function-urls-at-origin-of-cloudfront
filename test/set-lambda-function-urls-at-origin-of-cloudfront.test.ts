740b84d5a5790f1f67b11bf7b59e23e651e16928
mi-kobaaa <mi.kobayashi0920@gmail.com>
2022-06-30 20:48:16 +0900

    add cdk project

---
+++ test/set-lambda-function-urls-at-origin-of-cloudfront.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SetLambdaFunctionUrlsAtOriginOfCloudfront from '../lib/set-lambda-function-urls-at-origin-of-cloudfront-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/set-lambda-function-urls-at-origin-of-cloudfront-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SetLambdaFunctionUrlsAtOriginOfCloudfront.SetLambdaFunctionUrlsAtOriginOfCloudfrontStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
